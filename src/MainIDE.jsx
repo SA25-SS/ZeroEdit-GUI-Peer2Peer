cd2dc315100d83e89f2805aef2917016a5485e8a
Shreyas Ashtamkar <shreyu@programmer.net>
2025-04-17 15:18:31 +0530

    Cleanup

--- src/MainIDE.jsx
+++ src/MainIDE.jsx
@@ -9,19 +9,37 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Container, Row } from 'react-bootstrap';
 
 //Import the React framework
-import React from 'react';
+import React, { useState } from 'react';
 
 // Importing components
 import MainScreen from './components/MainScreen/MainScreen';
 import SideBar from './components/SideBar/SideBar';
 
-//Router
-function MainIDE({ DarkTheme, IDEVars }) {
+function MainIDE({ DarkTheme }) {
+    const [fileName, setFileName] = useState("somethingcool.js");
+    const [editorContent, setEditorContent] = useState("console.log('Something Cool')");
+    const [outputContent, setOutputContent] = useState("Something Cool");
+
+    const IDEVars = {
+        fileName: {
+            value: fileName,
+            set: setFileName
+        },
+        editorContent: {
+            value: editorContent,
+            set: setEditorContent
+        },
+        outputContent: {
+            value: outputContent,
+            set: setOutputContent
+        },
+    }
+
     return (
-        <Container className={`App px-0 ${(DarkTheme.global.get() && "bg-dark text-light") || "bg-light text-dark"}`} fluid>
+        <Container className={`App px-0 ${(DarkTheme.global.value && "bg-dark text-light") || "bg-light text-dark"}`} fluid>
             <Row className='mx-0'>
-                <SideBar colSize={2} globalThemeDark={DarkTheme.global.get()} openFileName={IDEVars.fileName.get()}/>
-                <MainScreen colSize={10} DarkTheme={DarkTheme} IDEVars={IDEVars}/>
+                <SideBar colSize={2} DarkTheme={DarkTheme} IDEVars={IDEVars} />
+                <MainScreen colSize={10} DarkTheme={DarkTheme} IDEVars={IDEVars} />
             </Row>
         </Container>
     );
