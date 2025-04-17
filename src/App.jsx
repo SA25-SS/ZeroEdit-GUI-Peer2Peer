6c142e35dbd07baa7984cac082c3c263e8d83849
Shreyas Ashtamkar <shreyu@programmer.net>
2025-04-17 15:18:13 +0530

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,22 @@ import MainIDE from './MainIDE';
 
 import { loadSavedGlobalTheme, saveGlobalTheme, loadSavedEditorTheme, saveEditorTheme } from "./utils"
 
+//Router
 function App() {
 	// Dark Mode Settings
 	const [globalThemeDark, setGlobalThemeDark] = useState(loadSavedGlobalTheme());
 	const [editorThemeDark, setEditorThemeDark] = useState(loadSavedEditorTheme());
 
-	const [fileName, setFileName] = useState("somethingcool.js");
-	const [editorContent, setEditorContent] = useState("console.log('Something Cool')");
-	const [outputContent, setOutputContent] = useState("Something Cool");
+	const DarkTheme = {
+		global:{
+			value:globalThemeDark, 
+			set:setGlobalThemeDark
+		},
+		editor:{
+			value:editorThemeDark, 
+			set:setEditorThemeDark
+		}
+	};
 
 	// Save to localStorage whenever it changes
 	useEffect(() => saveGlobalTheme(globalThemeDark), [globalThemeDark]);
@@ -29,31 +37,7 @@ function App() {
 					path="/"
 					element={
 						<MainIDE
-							DarkTheme={{
-								global:{
-									get:()=>globalThemeDark, 
-									set:setGlobalThemeDark
-								},
-								editor:{
-									get:()=>editorThemeDark, 
-									set:setEditorThemeDark
-								}
-							}}
-
-							IDEVars={{
-								fileName:{
-									get:()=>fileName,
-									set:setFileName
-								},
-								editorContent:{
-									get:()=>editorContent,
-									set:setEditorContent
-								},
-								outputContent:{
-									get:()=>outputContent,
-									set:setOutputContent
-								},
-							}}
+							DarkTheme={DarkTheme}
 						/>
 					}
 				/>
