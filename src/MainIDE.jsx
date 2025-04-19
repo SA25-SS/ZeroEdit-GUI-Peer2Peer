03df17f27f871179e7ffd986ac54b1c639ca50d3
Shreyas Ashtamkar <shreyu@programmer.net>
2025-04-19 12:24:48 +0530

    Future Compat

--- src/MainIDE.jsx
+++ src/MainIDE.jsx
@@ -20,29 +20,33 @@ import { useDocument } from '@automerge/automerge-repo-react-hooks';
 import { updateText } from '@automerge/automerge/next';
 
 function MainIDE({ DarkTheme, docUrl }) {
-    const [doc, changeDoc] = useDocument(docUrl);
+    // const [fileName, _setFileName] = useState("somethingcool.js");
+    // const [editorContent, _setEditorContent] = useState("console.log('Something Cool')");
     const [outputContent, setOutputContent] = useState("Something Cool");
 
-    const setFileName = (value) => changeDoc((d) => {
-        d.fileName = value;
-    });
-    
-    const setEditorContent = (value) => changeDoc((d) => {
-        d.fileContent = value;
-    });
+    // Automerge Doc
+    const [doc, changeDoc] = useDocument(docUrl);
+
+    const setFileName = (value) => {
+        changeDoc((d) => { d.fileName = value; });
+        // _setFileName(value);
+    };
+
+    const setEditorContent = (value) => {
+        changeDoc((d) => { d.fileContent = value; });
+        // _setEditorContent(value);
+    };
 
     const IDEVars = {
         fileName: {
+            // value: doc?.fileName ?? fileName,
             value: doc?.fileName ?? "somethingcool.js",
             set: setFileName
         },
         editorContent: {
+            // value: doc?.fileContent ?? editorContent,
             value: doc?.fileContent ?? "console.log('Something Cool')",
             set: setEditorContent
-        },
-        outputContent: {
-            value: outputContent,
-            set: setOutputContent
         },
     }
 
